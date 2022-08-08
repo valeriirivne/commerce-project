99a5ecedc930996a4ad5255af7b3a9a5771da404
vally <valerii.rivne@gmail.com>
2022-08-08 23:50:57 +0300

    added for the first time

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import { Routes, Route } from 'react-router-dom';
+
+import Navigation from './routes/navigation/navigation.component';
+import Home from './routes/home/home.component';
+import Authentication from './routes/authentication/authentication.component';
+
+function Shop() {
+  return (
+    <div>
+      <h1>I am the shop</h1>
+    </div>
+  );
+}
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />}></Route>
+        <Route path="shop" element={<Shop />} />
+        <Route path="auth" element={<Authentication />} />
+      </Route>
+    </Routes>
+  );
+};
+
+export default App;
+
+// Crud Read means get
